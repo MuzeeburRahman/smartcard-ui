7664d1553893e36fc67f4fc59ac3d1b887e1bc61
MuzeeburRahman Farooqi <muzeeb@MuzeeburRahmans-MacBook-Pro.local>
2020-07-13 00:36:51 +0530

    updated app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 2d919f666dcaabd724ca306a851a5a11b6c8cf94
 
 @NgModule({
     imports: [
@@ -26,12 +21,7 @@ import { LoginComponent } from './login';
         ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
-<<<<<<< HEAD
-        NgbModule,
-        
-=======
         NgbModule
->>>>>>> 2d919f666dcaabd724ca306a851a5a11b6c8cf94
     ],
     declarations: [
         AppComponent,
